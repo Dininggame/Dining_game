ff9cad4ccf442f445b973ddde914ed29daedd184
Isaraa <isaraa.p@ku.th>
2023-05-12 16:55:55 +0700

    Added More UI

--- Constants.java
+++
@@ -1,5 +0,0 @@
-public class Constants {
-    public static final int WINDOW_WIDTH = 800;
-    public static final int WINDOW_HEIGHT = 600;
-    public static final String Name = "Diner furious";
-}
